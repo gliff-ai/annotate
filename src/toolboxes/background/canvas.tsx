9c5325a7ff0d762cc50c6f931e0d11644f95d14e
Chas Nelson <cvs@chasnelson.co.uk>
2021-03-15 11:39:26 +0000

    feat: use react canvas draws draw WIP

--- src/toolboxes/background/canvas.tsx
+++ src/toolboxes/background/canvas.tsx
@@ -1,10 +1,10 @@
 import React from "React";
-import {Component} from "React";
+import { Component } from "React";
 
 import { BaseCanvas, Props as BaseProps } from "../../baseCanvas";
 
 interface Props extends BaseProps {
-    imgSrc?: string;
+  imgSrc?: string;
 }
 
 interface State {
@@ -13,45 +13,48 @@ interface State {
 }
 
 export class BackgroundCanvas extends Component<Props> {
-    private baseCanvas: any;
-    // private canvasContainer: HTMLDivElement;
-  
-    // private canvasContext: CanvasRenderingContext2D;
-    // private canvasObserver: ResizeObserver;
-  
-    // private coordSystem: CoordinateSystem;
-  
-    // private zoomExtents: Extents;
-  
-    constructor(props: Props) {
-      super(props);
-      this.imgSrc = props.imgSrc
-      this.imgObj = ImageData()
-      this.imgObj.src = this.imgSrc
-     
-    }
+  private baseCanvas: any;
+  // private canvasContainer: HTMLDivElement;
 
-    private addImageToCanvas = (
-        imgObj:ImageData
-      ): void => {
-        let width:number = imgObj.width
-        let height:number = imgObj.height
-        const imageData = this.baseCanvas.canvasContext.createImageData(width, height);
-        imageData.data.set(array);
-        this.baseCanvas.canvasContext.putImageData(imageData, 0, 0);
-      };
-      
-    componentDidMount = (): void => {
-        console.log("Mounted background");
-        console.log(this.baseCanvas);
-        console.log(this.baseCanvas?.canvas);
-    }
+  // private canvasContext: CanvasRenderingContext2D;
+  // private canvasObserver: ResizeObserver;
 
-    render() {
-        return (<BaseCanvas 
-            ref={(baseCanvas) => (this.baseCanvas = baseCanvas)}
-            name="background" 
-            zoomExtents={{min: 0.3, max: 3}}/>)
+  // private coordSystem: CoordinateSystem;
 
-    }
+  // private zoomExtents: Extents;
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  private drawImage = () => {
+    if (!this.props.imgSrc) return;
+
+    // Load the image
+    this.image = new Image();
+
+    // Prevent SecurityError "Tainted canvases may not be exported." #70
+    this.image.crossOrigin = "anonymous";
+
+    // Draw the image once loaded
+    this.image.onload = this.redrawImage;
+    this.image.src = this.props.imgSrc;
+  };
+
+  componentDidMount = (): void => {
+    console.log("Mounted background");
+    console.log(this.baseCanvas);
+    console.log(this.baseCanvas?.canvas);
+    this.drawImage(this.props.imgSrc);
+  };
+
+  render() {
+    return (
+      <BaseCanvas
+        ref={(baseCanvas) => (this.baseCanvas = baseCanvas)}
+        name="background"
+        zoomExtents={{ min: 0.3, max: 3 }}
+      />
+    );
+  }
 }
