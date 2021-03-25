0cd3c739d6b3656b634fcf36dd280356ad1e222f
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-03-25 17:35:46 +0000

    fix: apply vscode linting

--- src/upload3DImage.tsx
+++ src/upload3DImage.tsx
@@ -1,43 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import React, { Component, ReactNode } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-    },
-    input: {
-      display: 'none',
-    },
-  }),
-);
+interface UploadImage {
+  imageFile?: File;
+}
 
-export default function UploadButtons():any {
+interface UploadState {
+  imageFile?: File;
+}
 
-  const classes = useStyles();
+export default class Upload3DImage extends Component<UploadImage, UploadState> {
+  constructor(props: UploadImage) {
+    super(props);
+    this.state = {
+      imageFile: null,
+    };
+  }
 
-  const [image,setImage] = useState([]);
-
-  return (
-    <div className={classes.root}>
-      <input
-        accept="image/*"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      
-      <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
-      <label htmlFor="icon-button-file">
-        <IconButton aria-label="upload picture" component="span">
-          <PhotoCamera />
-        </IconButton>
-      </label>
-    </div>
-  );
+  render = (): ReactNode => {
+    return (
+      <div>
+        <input
+          accept="image/*"
+          id="icon-button-file"
+          type="file"
+          style={{ display: "none" }}
+          onChange={(e) => {
+            console.log(e.target.files[0]);
+            this.setState({ imageFile: e.target.files[0] }, () => {
+              console.log(this.state.imageFile);
+            });
+          }}
+        />
+        <label htmlFor="icon-button-file">
+          <IconButton aria-label="upload picture" component="span">
+            <PhotoCamera />
+          </IconButton>
+        </label>
+      </div>
+    );
+  };
 }
