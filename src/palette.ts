1d7c6663da15379a7ab11d411ecb6a317825174b
Craig Cooper <c@c9r.dev>
2021-04-07 12:36:56 +0100

    fix: use material colours for drawing

--- src/palette.ts
+++ src/palette.ts
@@ -2,6 +2,8 @@ type RGBColor = [red: number, green: number, blue: number];
 
 // Muted qualitative colour scheme, colourblind safe, but lacking a clear red or medium blue.
 const palette: RGBColor[] = [
+  [68, 170, 153],
+  [136, 204, 238],
   [51, 34, 136],
   [17, 119, 51],
   [153, 153, 51],
@@ -11,8 +13,8 @@ const palette: RGBColor[] = [
   [170, 68, 153],
 ];
 
-const main: RGBColor = [136, 204, 238];
-const secondary: RGBColor = [68, 170, 153];
+const main: RGBColor = [245, 0, 87]; // Material dark theme secondary
+const secondary: RGBColor = [63, 81, 181]; // Material dark theme primary
 
 function getRGBString(color: RGBColor): string {
   return `rgb(${color.join(",")})`;
