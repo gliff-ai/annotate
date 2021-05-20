f4459cdd44aa03b7aee0619dc1bb02b697041254
philipjackson <philiptgjackson@gmail.com>
2021-05-20 18:25:01 +0100

    style: re-ordered methods in Annotations class, grouping get and set methods

--- src/annotation/index.ts
+++ src/annotation/index.ts
@@ -30,6 +30,25 @@ export class Annotations {
       }) - 1;
   };
 
+  getLabels = (): string[] => this.data[this.activeAnnotationID].labels;
+
+  getActiveAnnotationID = (): number => this.activeAnnotationID;
+
+  getActiveAnnotation = (): Annotation => this.data[this.activeAnnotationID];
+
+  getSplineForActiveAnnotation = (): Spline =>
+    this.data[this.activeAnnotationID].spline;
+
+  isActiveAnnotationEmpty = (): boolean =>
+    // Check whether the active annotation object contains any
+    // paintbrush or spline annotations.
+    this.data[this.activeAnnotationID].spline.coordinates.length === 0 &&
+    this.data[this.activeAnnotationID].brushStrokes.length === 0;
+
+  getAllAnnotations = (): Annotation[] => this.data;
+
+  length = (): number => this.data.length;
+
   addLabel = (newLabel: string): void => {
     if (!this.data[this.activeAnnotationID].labels.includes(newLabel)) {
       this.data[this.activeAnnotationID].labels.push(newLabel);
@@ -41,17 +60,6 @@ export class Annotations {
       this.activeAnnotationID
     ].labels.filter((label) => label !== existingLabel);
   };
-
-  getLabels = (): string[] => this.data[this.activeAnnotationID].labels;
-
-  getActiveAnnotationID = (): number => this.activeAnnotationID;
-
-  getActiveAnnotation = (): Annotation => this.data[this.activeAnnotationID];
-
-  getSplineForActiveAnnotation = (): Spline =>
-    this.data[this.activeAnnotationID].spline;
-
-  length = (): number => this.data.length;
 
   setActiveAnnotationID = (id: number): void => {
     this.activeAnnotationID = id;
@@ -68,14 +76,6 @@ export class Annotations {
   setActiveAnnotationToolbox = (newToolbox: string): void => {
     this.data[this.activeAnnotationID].toolbox = newToolbox;
   };
-
-  isActiveAnnotationEmpty = (): boolean =>
-    // Check whether the active annotation object contains any
-    // paintbrush or spline annotations.
-    this.data[this.activeAnnotationID].spline.coordinates.length === 0 &&
-    this.data[this.activeAnnotationID].brushStrokes.length === 0;
-
-  getAllAnnotations = (): Annotation[] => this.data;
 
   setSplineSpaceTimeInfo = (z?: number, t?: number): void => {
     // Set space and time data for spline of active annotation.
