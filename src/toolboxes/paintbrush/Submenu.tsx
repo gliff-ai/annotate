1a96e10f2fb41d2782a34b156a30674efbdcd8f1
Chas Nelson <github@chasnelson.co.uk>
2021-08-28 13:11:43 +0100

    feat: implement new paintbrush submenu design

--- src/toolboxes/paintbrush/Submenu.tsx
+++ src/toolboxes/paintbrush/Submenu.tsx
@@ -1,7 +1,13 @@
 import { ChangeEvent, ReactElement, MouseEvent } from "react";
-import { createStyles, makeStyles, Popover } from "@material-ui/core";
-import { BaseSlider } from "@/components/BaseSlider";
+import {
+  ButtonGroup,
+  createStyles,
+  makeStyles,
+  Paper,
+  Popover,
+} from "@material-ui/core";
 import { BaseIconButton } from "@gliff-ai/style";
+import { BaseSlider } from "@/components/BaseSlider";
 import { tooltips } from "@/components/tooltips";
 import { Sliders, SLIDER_CONFIG } from "./configSlider";
 import { usePaintbrushStore } from "./Store";
@@ -16,8 +22,14 @@ const useStyles = makeStyles(() =>
   createStyles({
     baseSlider: {
       width: "63px",
-      height: "345px",
+      height: "285px",
       textAlign: "center",
+    },
+    subMenu: {
+      display: "flex",
+      justifyContent: "space-between",
+      width: "136px",
+      background: "none",
     },
   })
 );
@@ -28,6 +40,7 @@ const Submenu = (props: Props): ReactElement => {
 
   function changeBrushRadius(e: ChangeEvent, value: number) {
     setPaintbrush({
+      brushType: paintbrush.brushType, // FIXME
       brushRadius: value,
     });
   }
@@ -38,26 +51,61 @@ const Submenu = (props: Props): ReactElement => {
     );
   }
 
+  function selectBrush() {
+    setPaintbrush({
+      brushType: tooltips.paintbrush.name,
+      brushRadius: paintbrush.brushRadius, // FIXME
+    });
+  }
+
+  function selectEraser() {
+    setPaintbrush({
+      brushType: tooltips.eraser.name,
+      brushRadius: paintbrush.brushRadius, // FIXME
+    });
+  }
+
   return (
-    <Popover
-      open={props.isOpen}
-      anchorEl={props.anchorElement}
-      onClose={props.onClose}
-    >
-      <div className={classes.baseSlider}>
-        <BaseSlider
-          value={paintbrush.brushRadius}
-          config={SLIDER_CONFIG[Sliders.brushRadius]}
-          onChange={() => changeBrushRadius}
-          showEndValues={false}
-        />
-        <BaseIconButton
-          tooltip={tooltips.fillbrush}
-          onClick={fillBrush}
-          fill={false}
-        />
-      </div>
-    </Popover>
+    <>
+      <Popover
+        open={props.isOpen}
+        anchorEl={props.anchorElement}
+        onClose={props.onClose}
+        PaperProps={{ classes: { root: classes.subMenu } }}
+      >
+        <ButtonGroup
+          orientation="vertical"
+          size="small"
+          id="paintbrush-toolbar"
+        >
+          <BaseIconButton
+            tooltip={tooltips.paintbrush}
+            onClick={selectBrush}
+            fill={paintbrush.brushType === tooltips.paintbrush.name}
+          />
+          <BaseIconButton
+            tooltip={tooltips.eraser}
+            onClick={selectEraser}
+            fill={paintbrush.brushType === tooltips.eraser.name}
+          />
+          <BaseIconButton
+            tooltip={tooltips.fillbrush}
+            onClick={fillBrush}
+            fill={false}
+          />
+        </ButtonGroup>
+        <Paper>
+          <div className={classes.baseSlider}>
+            <BaseSlider
+              value={paintbrush.brushRadius}
+              config={SLIDER_CONFIG[Sliders.brushRadius]}
+              onChange={() => changeBrushRadius}
+              showEndValues={false}
+            />
+          </div>
+        </Paper>
+      </Popover>
+    </>
   );
 };
 
