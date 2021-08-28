7c696dc20430135b4fc3ef2ba50a3b4d7a63dc0d
Chas Nelson <github@chasnelson.co.uk>
2021-08-28 13:10:32 +0100

    feat: add brushType to paintbrush store

--- src/toolboxes/paintbrush/Store.ts
+++ src/toolboxes/paintbrush/Store.ts
@@ -1,10 +1,16 @@
 import { createStore } from "@/store";
+import { tooltips } from "@/components/tooltips";
 
+// types will include: paintbrush, eraser
 interface PaintbrushData {
+  brushType: string;
   brushRadius: number;
 }
 
-const defaultPaintbrush: PaintbrushData = { brushRadius: 10 };
+const defaultPaintbrush: PaintbrushData = {
+  brushType: tooltips.paintbrush.name,
+  brushRadius: 10,
+};
 
 // we've created store with initial value. This can now be used anywhere and will share the value.
 export const [usePaintbrushStore] = createStore(defaultPaintbrush);
