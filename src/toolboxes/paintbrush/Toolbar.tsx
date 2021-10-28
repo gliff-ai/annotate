ae91ac0f7b0fe0e79bb6bab505d08128bbfc332f
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2021-10-28 15:26:29 +0100

    fix: Amended Paintbrush Name in IconButton (#428)

--- src/toolboxes/paintbrush/Toolbar.tsx
+++ src/toolboxes/paintbrush/Toolbar.tsx
@@ -344,7 +344,7 @@ class Toolbar extends Component<Props> {
       <ButtonGroup style={{ all: "revert" }}>
         <IconButton
           tooltip={{
-            name: "Paintbush",
+            name: "Paintbrush",
             ...getShortcut("paintbrush.selectBrush"),
           }}
           icon={icons.brush}
