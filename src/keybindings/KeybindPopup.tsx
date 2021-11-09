f09497afae8b5dad950d4116b47b9065f2e86477
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-11-09 10:49:04 +0100

    feat: add overflow to shortcuts modal

--- src/keybindings/KeybindPopup.tsx
+++ src/keybindings/KeybindPopup.tsx
@@ -7,12 +7,14 @@ const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
+  bottom: "-40%",
   transform: "translate(-50%, -50%)",
   width: 800,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+  overflow: "scroll",
 };
 
 const events = ["openKeybinds"] as const;
