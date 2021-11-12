b7f3ef91e85c3561755d486416dc50f13161005a
Philip Jackson <philiptgjackson@gmail.com>
2021-11-12 16:59:43 +0000

    fix: reverse the last commit

--- src/Toolboxes.ts
+++ src/Toolboxes.ts
@@ -1,9 +1,11 @@
 import { Tooltip } from "@gliff-ai/style";
 
 export const Toolboxes = {
+  background: "background",
   paintbrush: "paintbrush",
   spline: "spline",
   boundingBox: "boundingBox",
+  ui: "ui",
 } as const;
 
 export type Toolbox = typeof Toolboxes[keyof typeof Toolboxes];
