853ffa18620bcf79a333fecb1cf688bec5b1d99b
Craig Cooper <c@c9r.dev>
2021-11-29 08:29:18 +0000

    fix: seperate tests

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -18,17 +18,4 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ["/node_modules/", "dist/"],
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
-    // the package three contains ES6 modules, so it needs to be transpiled, so tell jest to ignore everything in node_modules except three
-    // (would ignore everything in node_modules by default: https://jestjs.io/docs/en/configuration.html#transformignorepatterns-arraystring )
-    "/node_modules/(?!three)/", // would normally be "/node_modules/"
-    "\\.pnp\\.[^\\/]+$", // unchanged from default
-  ],
-
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    "\\.s?css$": "<rootDir>/src/testUtils/cssmock.js", // allows Jest to not die when importing a module that imports CSS
-  },
 };
