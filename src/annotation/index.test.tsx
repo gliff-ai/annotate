b0d8ed68cd6e1186058a0d8f68a3780af21e490d
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-12-07 17:03:08 +0000

    feat: add test case for convert spline to paintbrush WIP

--- src/annotation/index.test.tsx
+++ src/annotation/index.test.tsx
@@ -1,10 +1,11 @@
 import { Toolboxes } from "@/Toolboxes";
 import { Annotations } from "./index";
 
-const prevLabel = "label2";
 let annotationsObject: Annotations;
+let radius = 0.5;
+let is3D = false;
 
-describe("labels assignment", () => {
+describe("Convert Splines to Paintbrush", () => {
   beforeEach(() => {
     annotationsObject = new Annotations();
   });
@@ -14,5 +15,25 @@ describe("labels assignment", () => {
     annotationsObject.addSplinePoint({ x: 2, y: 5 }, false);
     const coordinates = annotationsObject.getSplineCoordinates();
     expect(coordinates).toEqual([{ x: 2, y: 5 }]);
+  });
+
+  test("test convert spline to paintbrush", () => {
+    const coordinates = annotationsObject.getSplineCoordinates();
+    const color = annotationsObject.getActiveAnnotationColor();
+    const spaceTimeInfo = annotationsObject.getSplineSpaceTimeInfo();
+
+    const brushStroke = {
+      coordinates,
+      spaceTimeInfo,
+      brush: {
+        radius,
+        type: "paint",
+        color,
+        is3D,
+      },
+    };
+    annotationsObject.addAnnotation(Toolboxes.paintbrush);
+    annotationsObject.addSplinePoint({ x: 2, y: 5 }, false);
+    annotationsObject.addBrushStroke(brushStroke, false);
   });
 });
