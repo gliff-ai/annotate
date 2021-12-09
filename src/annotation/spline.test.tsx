18056fee136c82c4b74d0f267ac611bbc30f01c9
Christian Baidoo <37402968+ChrisBaidoo@users.noreply.github.com>
2021-12-09 14:15:23 +0100

    feat: Convert spline to paintbrush test (#504)
    * feat: set up test environment WIP
    
    * test: spline points unit test
    
    * merge in origin main
    
    * Revert "merge in origin main"
    
    This reverts commit 5f80722848a9b0a637b3f19f703439bcf6fc7d0e.
    
    * feat: add test case for convert spline to paintbrush WIP
    
    * feat: add more unit test
    
    * feat: rename to spline.test.tsx
    
    * feat: fix lint
    
    Co-authored-by: Craig Cooper <c@c9r.dev>

---
+++ src/annotation/spline.test.tsx
@@ -0,0 +1,55 @@
+import { Toolboxes } from "@/Toolboxes";
+import { Annotations } from "./index";
+
+let annotationsObject: Annotations;
+
+describe("Convert Splines to Paintbrush", () => {
+  beforeEach(() => {
+    annotationsObject = new Annotations();
+    annotationsObject.addAnnotation(Toolboxes.spline);
+  });
+
+  test("test add spline points", () => {
+    annotationsObject.addSplinePoint({ x: 2, y: 5 }, false);
+    const coordinates = annotationsObject.getSplineCoordinates();
+    expect(coordinates).toEqual([{ x: 2, y: 5 }]);
+  });
+
+  test("test convert spline to paintbrush", () => {
+    annotationsObject.addSplinePoint({ x: 2, y: 5 }, false);
+    annotationsObject.convertSplineToPaintbrush(0.5);
+    const brustrokesCord = annotationsObject.getBrushStrokeCoordinates();
+    expect(brustrokesCord).toEqual([{ x: 2, y: 5 }]);
+  });
+
+  // Should we be testing 3D? We're passing in 3D to convertSplineToPaintbrush but it doesn't seem to be doing anything
+  // We only set radius to 0.5 when converting splines to paintbrush should this be the default number?
+
+  test("test closespline", () => {
+    annotationsObject.addSplinePoint({ x: 1, y: 1 }, false);
+    annotationsObject.addSplinePoint({ x: 10, y: 1 }, false);
+    annotationsObject.addSplinePoint({ x: 10, y: 10 }, false);
+    annotationsObject.setSplineClosed(true);
+    annotationsObject.convertSplineToPaintbrush(0.5);
+    const brustrokesCord = annotationsObject.getBrushStrokeCoordinates();
+    expect(brustrokesCord).toEqual([
+      { x: 1, y: 1 },
+      { x: 10, y: 1 },
+      { x: 10, y: 10 },
+      { x: 1, y: 1 },
+    ]);
+  });
+
+  test("test openSpline", () => {
+    annotationsObject.addSplinePoint({ x: 1, y: 1 }, false);
+    annotationsObject.addSplinePoint({ x: 10, y: 1 }, false);
+    annotationsObject.addSplinePoint({ x: 10, y: 10 }, false);
+    annotationsObject.convertSplineToPaintbrush(0.5);
+    const brustrokesCord = annotationsObject.getBrushStrokeCoordinates();
+    expect(brustrokesCord).toEqual([
+      { x: 1, y: 1 },
+      { x: 10, y: 1 },
+      { x: 10, y: 10 },
+    ]);
+  });
+});
