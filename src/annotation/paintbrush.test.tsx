90c3743b259ca02fa9e54743835ff6fcb2f6103b
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-12-10 17:27:10 +0000

    feat: add fill brush test case

--- src/annotation/paintbrush.test.tsx
+++ src/annotation/paintbrush.test.tsx
@@ -1,6 +1,7 @@
 import { Toolboxes } from "@/Toolboxes";
 import { BrushStroke } from "@/toolboxes/paintbrush";
 import { Annotations } from "./index";
+import fillBrush from "./fillBrushData";
 
 let annotationsObject: Annotations;
 
@@ -13,7 +14,7 @@ const brushStrokes: BrushStroke[] = [
     ],
     spaceTimeInfo: { z: 0, t: 0 },
     brush: {
-      color: undefined,
+      color: "rgba(136,204,238, 1)",
       radius: 20,
       type: "paint",
       is3D: false,
@@ -29,12 +30,18 @@ describe("Fill Active Paintbrush", () => {
 
   test("test draw brushstrokes", () => {
     annotationsObject.addBrushStroke(brushStrokes[0]);
-    annotationsObject.fillBrush(0, false);
     const strokes = annotationsObject.getBrushStrokeCoordinates();
     expect(strokes).toEqual([
       { x: 100, y: 100 },
       { x: 200, y: 200 },
       { x: 200, y: 100 },
     ]);
+  });
+
+  test("test fill paintbrush", () => {
+    annotationsObject.addBrushStroke(brushStrokes[0]);
+    annotationsObject.fillBrush(0, false);
+    const brushStrokesAfterFill = annotationsObject.getAllAnnotations()[0];
+    expect(brushStrokesAfterFill).toEqual(fillBrush);
   });
 });
