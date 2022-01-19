57de8b640d892311d3bab25e1a5356fe55601a15
Christian Baidoo <chris_pens@hotmail.co.uk>
2022-01-19 16:13:59 +0000

    feat: run prettier to fix lint error

--- src/toolboxes/labels/Submenu.tsx
+++ src/toolboxes/labels/Submenu.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, MouseEvent } from "react";
 import { Grid, Typography, Popover, Card, Paper, Theme } from "@mui/material";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 import { Labels, Props as LabelsProps } from "./Labels";
 
 const useStyles = makeStyles((theme: Theme) => ({
